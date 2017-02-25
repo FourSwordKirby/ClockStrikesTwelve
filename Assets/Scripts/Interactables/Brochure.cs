94334ce37ec4d07fbf0984005573d63f8af3da1c
FourSwordKirby <rogliu26@gmail.com>
2017-02-25 18:04:56 -0500

    started making the door for the shower guy

---
+++ Assets/Scripts/Interactables/Brochure.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Brochure : Interactable
+{
+    //public InventoryItem inventoryItem;
+    public TextAsset Instructions;
+    private List<string> dialogComponents;
+    bool isBloody = false;
+    private SpriteRenderer spriteRenderer;
+    public Sprite originalSprite;
+    public Sprite bloodySprite;
+
+    void Awake()
+    {
+        dialogComponents = new List<string>(Instructions.text.Split('\n'));
+        dialogComponents = dialogComponents.Select(x => x.Trim()).ToList();
+        dialogComponents = dialogComponents.Where(x => x != "").ToList();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public override void Interact()
+    {
+        StartCoroutine(CharmGet());
+    }
+
+    IEnumerator CharmGet()
+    {
+        GameManager.instance.SuspendGame();
+        for (int i = 0; i < dialogComponents.Count; i++)
+        {
+            string[] dialogPieces = dialogComponents[i].Split(new string[] { " : " }, System.StringSplitOptions.None);
+            string speaker = "";
+            string dialog = "";
+            if (dialogPieces[0].EndsWith(".wav")){
+                GameManager.instance.playSound(SoundType.Item, dialogPieces[0].Substring(0, dialogPieces[0].Length - 4));
+            }
+            else{
+                if (dialogPieces.Count() > 1)
+                {
+                    speaker = dialogPieces[0];
+                    dialog = dialogPieces[1];
+                }
+                else
+                {
+                    dialog = dialogPieces[0];
+                }
+                UIController.instance.dialog.displayDialog(dialog, speaker);
+                while (!UIController.instance.dialog.dialogCompleted)
+                {
+                    yield return new WaitForSeconds(0.1f);
+                }
+                //Replace this with things in the control set
+                while (!Controls.confirmInputHeld())
+                {
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
+        }
+        UIController.instance.dialog.closeDialog();
+        GameManager.instance.UnsuspendGame();
+
+        if (!(isBloody)){
+            spriteRenderer.sprite = bloodySprite;
+            //Sprite bloodySprite = Resources.Load<Sprite>("Assets/Sprites/Objects/BloodyBrochure");
+            ////Sprite originalSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+            ////Sprite bloodyBrochure = Sprite.Create(originalSprite.texture, originalSprite.rect, originalSprite.pivot);
+            //gameObject.GetComponent<SpriteRenderer>().sprite = bloodySprite;
+            isBloody = true;
+        }
+
+
+        //We picked up the object, do stuff to it
+        //Destroy(this.gameObject);
+        //GameManager.instance.playSound(SoundType.Item, "ItemGet");
+
+        //Player.instance.items.Add(inventoryItem);
+        Player.instance.currentInteractable = null;
+        StartCoroutine(Player.instance.HideSymbol());
+        yield return null;
+    }
+}
---
+++ Assets/ShowerRoomDoor.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ShowerRoomDoor : Interactable
+{
+    public TextAsset ShowerDoorResponse1;
+    public TextAsset ShowerDoorResponse2;
+    public TextAsset ShowerDoorResponse3;
+    public TextAsset ShowerDoorResponse4;
+    public TextAsset ShowerDoorResponse5;
+    public TextAsset RequestMaintenence;
+
+    private List<string> dialogComponents;
+
+    void Start()
+    {
+        if(QuestManager.instance.toiletsFlushed == 0)
+            dialogComponents = new List<string>(ShowerDoorResponse1.text.Split('\n'));
+        else if (QuestManager.instance.toiletsFlushed == 1)
+            dialogComponents = new List<string>(ShowerDoorResponse2.text.Split('\n'));
+        else if (QuestManager.instance.toiletsFlushed == 2)
+            dialogComponents = new List<string>(ShowerDoorResponse3.text.Split('\n'));
+        else if (QuestManager.instance.toiletsFlushed == 3)
+            dialogComponents = new List<string>(ShowerDoorResponse4.text.Split('\n'));
+        else if (QuestManager.instance.toiletsFlushed == 4)
+            dialogComponents = new List<string>(ShowerDoorResponse5.text.Split('\n'));
+        else if (QuestManager.instance.toiletsFlushed == 5)
+        {
+            dialogComponents = new List<string>(RequestMaintenence.text.Split('\n'));
+        }
+
+        dialogComponents = dialogComponents.Select(x => x.Trim()).ToList();
+        dialogComponents = dialogComponents.Where(x => x != "").ToList();
+    }
+
+    public override void Interact()
+    {
+        StartCoroutine(DoorResponse());
+    }
+
+    IEnumerator DoorResponse()
+    {
+        GameManager.instance.SuspendGame();
+        for (int i = 0; i < dialogComponents.Count; i++)
+        {
+            string[] dialogPieces = dialogComponents[i].Split(new string[] { " : " }, System.StringSplitOptions.None);
+            string speaker = "";
+            string dialog = "";
+            if (dialogPieces.Count() > 1)
+            {
+                speaker = dialogPieces[0];
+                dialog = dialogPieces[1];
+            }
+            else
+                dialog = dialogPieces[0];
+            UIController.instance.dialog.displayDialog(dialog, speaker);
+
+            yield return new WaitForSeconds(0.1f);
+            while (!UIController.instance.dialog.dialogCompleted)
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+            while (!Controls.confirmInputHeld())
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+
+        if (QuestManager.instance.toiletsFlushed == 5)
+        {
+            QuestManager.instance.maintencenceRequestCalled = true;
+        }
+
+        yield return null;
+    }
+}
+
