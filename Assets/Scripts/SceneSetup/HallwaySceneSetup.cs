300229a5b8c618b63014d2c4678c87f566338b36
kaikue <kvk@pobox.com>
2017-02-25 16:01:39 -0500

    Add lock on door

---
+++ Assets/Scripts/SceneSetup/HallwaySceneSetup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HallwaySceneSetup : MonoBehaviour {
+
+    public bool toiletPuzzleComplete;
+    public GameObject doorLock;
+
+	void Start ()
+    {
+        if (UIController.instance.screenfader.fadeActive)
+        {
+            StartCoroutine(UIController.instance.screenfader.FadeIn());
+        }
+
+        if (toiletPuzzleComplete)
+        {
+            doorLock.SetActive(false);
+        }
+	}
+}
