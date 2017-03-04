915b4e9890be2632ea9209d146c864a2f7c221b7
Gkxd <gkxd@users.noreply.github.com>
2017-03-04 16:35:39 -0500

    Adding voice code/assets

---
+++ Assets/Voices/Voices.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Voices : MonoBehaviour
+{
+
+    public static Voices instance;
+
+    private AudioSource audioSource;
+    private Coroutine stopAudioRoutine;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        PlayClip(1);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private IEnumerator StopAudioSource(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        audioSource.Stop();
+    }
+
+    private void playClip(int clipId)
+    {
+        if (stopAudioRoutine != null)
+        {
+            StopCoroutine(stopAudioRoutine);
+        }
+        audioSource.time = 0.5f * clipId;
+        audioSource.Play();
+        stopAudioRoutine = StartCoroutine(StopAudioSource(0.45f));
+    }
+
+    /// <summary>
+    /// Plays a clip from the voices sound file. Long clips last for 0.25 seconds. Short clips last for 0.125 seconds.
+    /// <para/>Clips 0 - 2:   Low voice (long)
+    /// <para/>Clips 3 - 7:   Low voice(short)
+    /// <para/>Clips 8 - 10:  Medium voice(long)
+    /// <para/>Clips 11 - 15: Medium voice(short)
+    /// <para/>Clips 16 - 19: High voice(long)
+    /// <para/>Clips 20 - 32: High voice(short)
+    /// </summary>
+    /// <param name="clipId">The index of the clip to play</param>
+    public static void PlayClip(int clipId)
+    {
+        if (instance != null)
+        {
+            instance.playClip(clipId);
+        }
+    }
+}
