fc00e7e1bf70a1d91daa63fdd24ecdaff4db0851
kaikue <kvk@pobox.com>
2017-04-15 16:49:22 -0400

    More interaction toggle scripts

---
+++ Assets/Scripts/Interactables/LampTable.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class LampTable : Interactable
+{
+    public bool isOn;
+    public GameObject lamp;
+    private SpriteRenderer spriteRenderer;
+    public Sprite offSprite;
+    public Sprite onSprite;
+
+    void Awake()
+    {
+        spriteRenderer = lamp.GetComponent<SpriteRenderer>();
+    }
+
+    public override void Interact()
+    {
+        // Put interact code here
+        ToggleOn();
+    }
+
+    private void ToggleOn()
+    {
+        if (isOn)
+        {
+            spriteRenderer.sprite = offSprite;
+            isOn = false;
+        }
+        else
+        {
+            spriteRenderer.sprite = onSprite;
+            isOn = true;
+        }
+    }
+}
