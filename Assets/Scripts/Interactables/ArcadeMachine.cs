225e9092763849dd611e51a9b80249e94c9c2c5c
Zygnius <twjia1998@gmail.com>
2017-04-15 17:25:10 -0400

    Arcade room interactables

---
+++ Assets/Scripts/Interactables/ArcadeMachine.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ArcadeMachine : Interactable {
+    public Sprite On;
+    public Sprite Off;
+    private SpriteRenderer srender;
+    public TextAsset arcadeText;
+    private List<string> dialogComponents;
+
+    private void Awake()
+    {
+        srender = GetComponent<SpriteRenderer>();
+        dialogComponents = new List<string>(arcadeText.text.Split('\n'));
+        dialogComponents = dialogComponents.Select(x => x.Trim()).ToList();
+        dialogComponents = dialogComponents.Where(x => x != "").ToList();
+    }
+
+    public override void Interact()
+    {
+        StartCoroutine(arcade());
+    }
+
+    IEnumerator arcade()
+    {
+        srender.sprite = On;
+        GameManager.instance.SuspendGame();
+        for (int i = 0; i < dialogComponents.Count; i++)
+        {
+            string[] dialogPieces = dialogComponents[i].Split(new string[] { " : " }, System.StringSplitOptions.None);
+            string speaker = "";
+            string dialog = "";
+            if (dialogPieces.Count() > 1)
+            {
+                speaker = dialogPieces[0];
+                dialog = dialogPieces[1];
+            }
+            else
+                dialog = dialogPieces[0];
+            UIController.instance.dialog.displayDialog(dialog, speaker);
+            while (!UIController.instance.dialog.dialogCompleted)
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+            //Replace this with things in the control set
+            while (!Controls.confirmInputHeld())
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        UIController.instance.dialog.closeDialog();
+        GameManager.instance.UnsuspendGame();
+        srender.sprite = Off;
+    }
+}
---
+++ Assets/Scripts/Interactables/VendingMachine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class VendingMachine : Interactable {
+    
+    public TextAsset arcadeText;
+    private List<string> dialogComponents;
+
+    private void Awake()
+    {
+        dialogComponents = new List<string>(arcadeText.text.Split('\n'));
+        dialogComponents = dialogComponents.Select(x => x.Trim()).ToList();
+        dialogComponents = dialogComponents.Where(x => x != "").ToList();
+    }
+
+    public override void Interact()
+    {
+        StartCoroutine(arcade());
+    }
+
+    IEnumerator arcade()
+    {
+        GameManager.instance.SuspendGame();
+        for (int i = 0; i < dialogComponents.Count; i++)
+        {
+            string[] dialogPieces = dialogComponents[i].Split(new string[] { " : " }, System.StringSplitOptions.None);
+            string speaker = "";
+            string dialog = "";
+            if (dialogPieces.Count() > 1)
+            {
+                speaker = dialogPieces[0];
+                dialog = dialogPieces[1];
+            }
+            else
+                dialog = dialogPieces[0];
+            UIController.instance.dialog.displayDialog(dialog, speaker);
+            while (!UIController.instance.dialog.dialogCompleted)
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+            //Replace this with things in the control set
+            while (!Controls.confirmInputHeld())
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        UIController.instance.dialog.closeDialog();
+        GameManager.instance.UnsuspendGame();
+    }
+}
