c15f8674e167650516502e9c7b95806ac44750ad
FourSwordKirby <rogliu26@gmail.com>
2017-04-21 16:49:08 -0400

    cleaned up some of the excess dialog files. Implemented locks and what not in the hallway

---
+++ Assets/MomChildDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomChildDoor : Interactable
+{
+    public TextAsset spookyResponse;
+    public GameObject ooze;
+    public Collider2D lockCollisionBox;
+
+    private TextAsset currentDialog;
+
+    private void Update()
+    {
+        if (GameManager.instance.currentTime > GameManager.instance.timeLimit * 0.5f)
+        {
+            ooze.SetActive(true);
+            lockCollisionBox.enabled = true;
+        }
+        else
+        {
+            ooze.SetActive(false);
+            lockCollisionBox.enabled = false;
+        }
+    }
+
+    public override void Interact()
+    {
+        SetCurrentDialog();
+        StartCoroutine(Talk());
+    }
+
+    private void SetCurrentDialog()
+    {
+        currentDialog = spookyResponse;
+    }
+
+    IEnumerator Talk()
+    {
+        yield return Dialog.DisplayDialog(Dialog.CreateDialogComponents(currentDialog.text));
+    }
+}
