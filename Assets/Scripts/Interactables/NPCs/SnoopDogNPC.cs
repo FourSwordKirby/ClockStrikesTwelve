7f1d22ce707a01ea61d2fcc6999cc5d6fa7be877
FourSwordKirby <rogliu26@gmail.com>
2017-04-21 15:00:17 -0400

    made template classes for the other NPCs

---
+++ Assets/Scripts/Interactables/NPCs/SnoopDogNPC.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnoopDogNPC : NPC
+{
+    override public void Interact()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Interactables/NPCs/WriterNPC.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WriterNPC : NPC
+{
+    override public void Interact()
+    {
+
+    }
+}
