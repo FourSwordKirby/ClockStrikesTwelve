74025e4729c0442311210e4b6bb1a52dbadd03c2
kaikue <kvk@pobox.com>
2017-04-21 16:39:01 -0400

    Add mom and child NPCs

--- Assets/Scripts/Interactables/NPCs/ChildNPC.cs
+++ Assets/Scripts/Interactables/NPCs/ChildNPC.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class ChildNPC : NPC
 {
-    override public void Interact()
-    {
+    public TextAsset PlaceholderText;
 
+    private TextAsset currentDialog;
+
+    public override void Interact()
+    {
+        base.Interact();
+        SetCurrentDialog();
+        StartCoroutine(Talk());
+    }
+
+    private void SetCurrentDialog()
+    {
+        //TODO: this is just placeholder
+        if (true)
+        {
+            currentDialog = PlaceholderText;
+        }
+    }
+
+    IEnumerator Talk()
+    {
+        yield return Dialog.DisplayDialog(Dialog.CreateDialogComponents(currentDialog.text));
     }
 }
--- Assets/Scripts/Interactables/NPCs/MomNPC.cs
+++ Assets/Scripts/Interactables/NPCs/MomNPC.cs
@@ -3,8 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MomNPC : NPC {
-    override public void Interact()
-    {
+    public TextAsset PlaceholderText;
 
+    private TextAsset currentDialog;
+
+    public override void Interact()
+    {
+        base.Interact();
+        SetCurrentDialog();
+        StartCoroutine(Talk());
+    }
+
+    private void SetCurrentDialog()
+    {
+        //TODO: this is just placeholder
+        if (true)
+        {
+            currentDialog = PlaceholderText;
+        }
+    }
+
+    IEnumerator Talk()
+    {
+        yield return Dialog.DisplayDialog(Dialog.CreateDialogComponents(currentDialog.text));
     }
 }
