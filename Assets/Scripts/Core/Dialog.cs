c83a56dc26a0f5932a0bccc540cddb981327c056
FourSwordKirby <rogliu26@gmail.com>
2017-04-21 17:48:19 -0400

    got the writers quest kind of working

--- Assets/Scripts/Core/Dialog.cs
+++ Assets/Scripts/Core/Dialog.cs
@@ -43,4 +43,112 @@ public class Dialog {
         UIController.instance.dialog.closeDialog();
         GameManager.instance.UnsuspendGame();
     }
+
+    public static IEnumerator DisplayPrompt(List<string> promptComponents, string correctAnswer, List<string> correctComponents, List<string> incorrectComponents)
+    {
+        GameManager.instance.SuspendGame();
+        for (int i = 0; i < promptComponents.Count; i++)
+        {
+            string[] dialogPieces = promptComponents[i].Split(new string[] { " : " }, System.StringSplitOptions.None);
+            string speaker = "";
+            string dialog = "";
+            if (dialogPieces.Length > 1)
+            {
+                speaker = dialogPieces[0];
+                dialog = dialogPieces[1];
+            }
+            else
+                dialog = dialogPieces[0];
+            Debug.Log("ABout to displat");
+            UIController.instance.dialog.displayDialog(dialog, speaker);
+            while (!UIController.instance.dialog.dialogCompleted)
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+            //Replace this with things in the control set
+            while (!Controls.confirmInputHeld())
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        UIController.instance.dialog.closeDialog();
+
+        UIController.instance.passwordPrompt.openPassword();
+
+        yield return new WaitForSeconds(1.0f);
+        while (true)
+        {
+            if (Controls.cancelInputHeld() || Controls.confirmInputHeld())
+            {
+                if (UIController.instance.passwordPrompt.getAnswer().Length > 0)
+                {
+                    break;
+                }
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        UIController.instance.passwordPrompt.closePassword();
+
+        string enteredAnswer = UIController.instance.passwordPrompt.getAnswer();
+        if (enteredAnswer == correctAnswer)
+        {
+            for (int i = 0; i < correctComponents.Count; i++)
+            {
+                string[] dialogPieces = correctComponents[i].Split(new string[] { " : " }, System.StringSplitOptions.None);
+                string speaker = "";
+                string dialog = "";
+                if (dialogPieces.Count() > 1)
+                {
+                    speaker = dialogPieces[0];
+                    dialog = dialogPieces[1];
+                }
+                else
+                    dialog = dialogPieces[0];
+                UIController.instance.dialog.displayDialog(dialog, speaker);
+
+                yield return new WaitForSeconds(0.1f);
+                while (!UIController.instance.dialog.dialogCompleted)
+                {
+                    yield return new WaitForSeconds(0.1f);
+                }
+                //Replace this with things in the control set
+                while (!Controls.confirmInputHeld())
+                {
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < incorrectComponents.Count; i++)
+            {
+                string[] dialogPieces = incorrectComponents[i].Split(new string[] { " : " }, System.StringSplitOptions.None);
+                string speaker = "";
+                string dialog = "";
+                if (dialogPieces.Count() > 1)
+                {
+                    speaker = dialogPieces[0];
+                    dialog = dialogPieces[1];
+                }
+                else
+                    dialog = dialogPieces[0];
+                UIController.instance.dialog.displayDialog(dialog, speaker);
+
+                yield return new WaitForSeconds(0.1f);
+                while (!UIController.instance.dialog.dialogCompleted)
+                {
+                    yield return new WaitForSeconds(0.1f);
+                }
+                //Replace this with things in the control set
+                while (!Controls.confirmInputHeld())
+                {
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
+        }
+        UIController.instance.dialog.closeDialog();
+
+
+        GameManager.instance.UnsuspendGame();
+    }
 }
--- Assets/Scripts/Interactables/NPCs/WriterNPC.cs
+++ Assets/Scripts/Interactables/NPCs/WriterNPC.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class WriterNPC : NPC
 {
-    public TextAsset PlaceholderText;
+    public TextAsset writerIntro;
+    public TextAsset writerPrompt1;
+    public TextAsset writerCorrect1;
+    public TextAsset writerWrong1;
+
+    public TextAsset writerPromptFail;
+
 
     private TextAsset currentDialog;
 
@@ -12,20 +18,32 @@ public class WriterNPC : NPC
     {
         base.Interact();
         SetCurrentDialog();
-        StartCoroutine(Talk());
+
+        if (currentDialog != writerPrompt1)
+            StartCoroutine(Talk());
+        else
+            StartCoroutine(Prompt(Dialog.CreateDialogComponents(writerPrompt1.text), 
+                                    "hi",
+                                    Dialog.CreateDialogComponents(writerCorrect1.text),
+                                    Dialog.CreateDialogComponents(writerWrong1.text)));
     }
 
     private void SetCurrentDialog()
     {
-        //TODO: this is just placeholder
-        if (true)
+        if (QuestManager.instance.tvOff)
         {
-            currentDialog = PlaceholderText;
+            if (QuestManager.instance.ideaCount == 0)
+                currentDialog = writerPrompt1;
         }
     }
 
     IEnumerator Talk()
     {
         yield return Dialog.DisplayDialog(Dialog.CreateDialogComponents(currentDialog.text));
+    }
+
+    IEnumerator Prompt(List<string> promptComponents, string correctAnswer, List<string> correctComponents, List<string> incorrectComponents)
+    {
+        yield return Dialog.DisplayPrompt(promptComponents, correctAnswer, correctComponents, incorrectComponents);
     }
 }
