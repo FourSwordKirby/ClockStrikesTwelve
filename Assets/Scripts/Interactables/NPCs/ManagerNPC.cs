0d4cfa26bd702993a3c4f28e4ac98273ebf334cf
kaikue <kvk@pobox.com>
2017-04-29 18:08:22 -0400

    Add manager animation

--- Assets/Scripts/Interactables/NPCs/ManagerNPC.cs
+++ Assets/Scripts/Interactables/NPCs/ManagerNPC.cs
@@ -11,13 +11,38 @@ public class ManagerNPC : NPC
     public TextAsset BoothIntro1;
     public TextAsset BoothIntro2;
 
+    public RuntimeAnimatorController AnimatorUp;
+    public RuntimeAnimatorController AnimatorDown;
+    public RuntimeAnimatorController AnimatorLeft;
+    public RuntimeAnimatorController AnimatorRight;
+
     private TextAsset currentDialog;
     
     public override void Interact()
     {
         base.Interact();
+        SetFacingAnim();
         SetCurrentDialog();
         StartCoroutine(Talk());
+    }
+
+    private void SetFacingAnim()
+    {
+        Parameters.InputDirection dir = PositionsToDirection(Player.instance.transform.position);
+        if (dir == Parameters.InputDirection.N)
+            SetAnimator(AnimatorUp);
+        if (dir == Parameters.InputDirection.S)
+            SetAnimator(AnimatorDown);
+        if (dir == Parameters.InputDirection.W)
+            SetAnimator(AnimatorLeft);
+        if (dir == Parameters.InputDirection.E)
+            SetAnimator(AnimatorRight);
+    }
+
+    private void SetAnimator(RuntimeAnimatorController source)
+    {
+        Animator animator = gameObject.GetComponent<Animator>();
+        animator.runtimeAnimatorController = source;
     }
 
     private void SetCurrentDialog()
