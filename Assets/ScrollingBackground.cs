8303e9a88091d6b3381f74c44004c89d9c984dc9
Dunjunmstr <dunjunmstr@gmail.com>
2017-04-30 21:36:19 -0400

    Made the background scroll

---
+++ Assets/ScrollingBackground.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeZ;
+    private float maxSize;
+    private float originalPos;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        originalPos = gameObject.GetComponent<RectTransform>().anchoredPosition[1];
+        maxSize = 2976;//gameObject.GetComponent<RectTransform>().rect.height / 2;
+        print(maxSize);
+        //startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 coordinates = gameObject.GetComponent<RectTransform>().anchoredPosition;
+        float properYCoordinate = ((coordinates[1] + scrollSpeed - originalPos) % maxSize) + originalPos;
+        gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(coordinates[0], properYCoordinate);
+        //gameObject.GetComponent<RectTransform>().position[0] = 1000;
+        //float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        //transform.position = startPosition + Vector3.forward * newPosition;
+    }
+}
